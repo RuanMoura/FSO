2572127701756e634d1f3ba386bdb647624d82e2
Antonio Ruan <ruan.moura@zerum.com>
2021-09-06 09:24:08 -0300

    :bulb: Add function to count execution time

--- trabalho1/trab.c
+++ trabalho1/trab.c
@@ -2,14 +2,46 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<sys/time.h>
+#include<signal.h>
+
+void trata_sinal(int s)
+{
+    signal(SIGUSR1, trata_sinal);
+}
 
 void shell(char *path, char *arg)
 {
     execl((const char *) path, (const char *) path, (const char *) arg, (char*) NULL);
 }
 
+void count_time()
+{
+    struct timeval initial_time, end_time;
+
+    gettimeofday(&initial_time, NULL);
+    pause();
+    gettimeofday(&end_time, NULL);
+
+    double initial_sec = (double) initial_time.tv_sec;
+    double initial_usec = (double) initial_time.tv_usec/1000000;
+    double end_sec = (double) end_time.tv_sec;
+    double end_usec = (double) end_time.tv_usec/1000000;
+
+    double initial = initial_sec + initial_usec;
+    double end = end_sec + end_usec;
+
+    double delta_time = end - initial;
+
+    printf("> Demorou %.1lf segundos, retornou 0\n", delta_time);
+    
+    kill(getpid(), SIGKILL);
+}
+
 int main()
 {
+    signal(SIGUSR1, trata_sinal);
+
     char path[255], arg[255];
     pid_t pid;
 
@@ -22,7 +54,17 @@ int main()
         }
         else
         {
-            wait(NULL);
+            pid = fork();
+            if(pid == 0)
+            {
+                count_time();
+            }
+            else
+            {
+                wait(NULL);
+                kill(pid, SIGUSR1);
+                wait(NULL);
+            }
         }
     }
     
