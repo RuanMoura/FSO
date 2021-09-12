62e9ceca7894b6b695202ccf1e8172808fb1a6e9
Ruan Moura <ruanmoura13@outlook.com>
2021-09-12 15:39:10 -0300

    :bulb: Catch child return code

--- trabalho1/trab.c
+++ trabalho1/trab.c
@@ -4,69 +4,73 @@
 #include<sys/wait.h>
 #include<sys/time.h>
 #include<signal.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<string.h>
 
-void trata_sinal(int s)
+int error = 0;
+struct timeval initial_time, end_time;
+pid_t child_pid;
+
+void trata_sinal(int);
+void shell(char *, char *);
+double count_time(struct timeval, struct timeval);
+
+
+int main()
 {
-    signal(SIGUSR1, trata_sinal);
+    char path[255], arg[255];
+    struct timeval init_prog, end_prog;
+
+    int status;
+
+    gettimeofday(&init_prog, NULL);
+    while (scanf(" %s %[^\n]%*c", path, arg) == 2)
+    {
+        error = 0;
+        child_pid = fork();
+        if(child_pid == 0)
+        {
+            shell(path, arg);
+        }
+        else
+        {
+            gettimeofday(&initial_time, NULL);
+            wait(&status);
+            gettimeofday(&end_time, NULL);
+            printf("> Demorou %.1lf segundos, retornou %d\n", count_time(initial_time, end_time), WEXITSTATUS(status));
+            fflush(stdout);
+        }
+    }
+    gettimeofday(&end_prog, NULL);
+    printf(">> O tempo total foi de %.1lf segundos\n", count_time(init_prog, end_prog));
+    fflush(stdout);
+    return 0;
 }
 
 void shell(char *path, char *arg)
 {
-    execl((const char *) path, (const char *) path, (const char *) arg, (char*) NULL);
+    int retorno = execl((const char *) path, (const char *) path, (const char *) arg, (char*) NULL);
+    if(retorno == -1)
+    {
+        printf("> Erro: %s\n", strerror(errno));
+        fflush(stdout);
+    }
+    fclose(stdin);
+    _exit(errno);
 }
 
-void count_time()
+double count_time(struct timeval initial_time, struct timeval end_time)
 {
-    struct timeval initial_time, end_time;
-
-    gettimeofday(&initial_time, NULL);
-    pause();
-    gettimeofday(&end_time, NULL);
-
     double initial_sec = (double) initial_time.tv_sec;
-    double initial_usec = (double) initial_time.tv_usec/1000000;
+    double initial_usec = (double) initial_time.tv_usec/1000000.0;
     double end_sec = (double) end_time.tv_sec;
-    double end_usec = (double) end_time.tv_usec/1000000;
+    double end_usec = (double) end_time.tv_usec/1000000.0;
 
     double initial = initial_sec + initial_usec;
     double end = end_sec + end_usec;
 
     double delta_time = end - initial;
 
-    printf("> Demorou %.1lf segundos, retornou 0\n", delta_time);
-    
-    kill(getpid(), SIGKILL);
-}
-
-int main()
-{
-    signal(SIGUSR1, trata_sinal);
-
-    char path[255], arg[255];
-    pid_t pid;
-
-    while (scanf(" %s %[^\n]%*c", path, arg) == 2)
-    {
-        pid = fork();
-        if(pid == 0)
-        {
-            shell(path, arg);
-        }
-        else
-        {
-            pid = fork();
-            if(pid == 0)
-            {
-                count_time();
-            }
-            else
-            {
-                wait(NULL);
-                kill(pid, SIGUSR1);
-                wait(NULL);
-            }
-        }
-    }
-    
-    return 0;
+    return delta_time;
 }
